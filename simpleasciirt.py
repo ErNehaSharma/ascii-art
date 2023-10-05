6e196f65e0d24360da15203e455a944187dd05fd
ErNehaSharma <129760892+ErNehaSharma@users.noreply.github.com>
2023-10-05 12:28:38 +0530

    Create simpleasciirt.py
    In this code, you can customize the ASCII art by modifying the "art" variable with your own design. The example above shows a simple cat face. Feel free to replace it with your unique design. When you run this code, it will print your customized ASCII art to the console.

---
+++ simpleasciirt.py
@@ -0,0 +1,12 @@
+def generate_unique_ascii_art():
+    # Customize your unique ASCII art pattern here
+    art = """
+    /\\_/\\
+   ( o.o )
+    > ^ <
+    """
+
+    print(art)
+
+if __name__ == "__main__":
+    generate_unique_ascii_art()
